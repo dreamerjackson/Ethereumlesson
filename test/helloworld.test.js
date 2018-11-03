af79fd083e08aba18726533bcb7abd184ae84a43
dreamerjackson <zhuimengshaonian04@gmail.com>
2018-11-03 20:46:57 +0800

    ...

--- test/helloworld.test.js
+++ test/helloworld.test.js
@@ -5,13 +5,22 @@ const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
     //  const helloworld  = require('../compile');//helloworld.bytecode  helloworld.interface
 const {bytecode,interface}  = require('../compile');
+const assert = require('assert');
+
+var helloworld;
 beforeEach( async()=>{
     fetchAccounts = await web3.eth.getAccounts();
-    console.log(fetchAccounts);
+    helloworld =  await   new web3.eth.Contract(JSON.parse(interface)).deploy({data:bytecode,arguments:['jonson']}).send({from:fetchAccounts[0],gas:'1000000'});
+    //console.log(fetchAccounts);
   //promise
 });
- 
+
 describe('HelloWorld',()=>{
 
-  it('deploy contract',()=>{})
+  it('deploy contract',()=>{
+
+  //  console.log(helloworld);
+
+    assert.ok(helloworld.options.address);
+  })
 })
